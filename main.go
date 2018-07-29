b0b5cfbfb5da052d7849ba6b0f77d1a7ae47fd2f
danielkvist <d94.zaragoza@gmail.com>
2018-07-29 16:32:52 +0200

    feat: a gopher is now printed in ASCII
    Now the main function reads a file (gopher-ascii.txt) from which it extracts an ASCII draw of the Golang project mascot.

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
 	s := strings.Join(os.Args[1:], " ")
+	d, err := ioutil.ReadFile("./gopher-ascii.txt")
 
-	fmt.Printf(" %s\n/ %s /\n %[1]s\n", strings.Repeat("-", len(s)+2), s)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf(" %s\n/ %s /\n %[1]s\n", strings.Repeat("~", len(s)+2), s)
+	fmt.Printf("%s%s\n", strings.Repeat(" ", len(s)/3), "###")
+	fmt.Printf("%s%s\n", strings.Repeat(" ", (len(s)/3)+2), "##")
+	fmt.Printf("%s%s\n", strings.Repeat(" ", (len(s)/3)+4), "#")
+	fmt.Print(string(d))
 }
