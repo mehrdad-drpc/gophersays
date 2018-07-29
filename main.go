ea4be81c2bf873501287c994ade979f790d28dec
danielkvist <d94.zaragoza@gmail.com>
2018-07-29 14:52:51 +0200

    refactor: code moved to the main func
    The code has been moved to the main func and the variable that defined the size of the string (sl) has been removed

--- main.go
+++ main.go
@@ -8,13 +8,6 @@ import (
 
 func main() {
 	s := strings.Join(os.Args[1:], " ")
-	sl := len(s)
 
-	talk(s, sl)
-}
-
-func talk(s string, sl int) {
-	d := strings.Repeat("-", sl+2)
-
-	fmt.Printf(" %s\n/ %s /\n %[1]s\n", d, s)
+	fmt.Printf(" %s\n/ %s /\n %[1]s\n", strings.Repeat("-", len(s)+2), s)
 }
